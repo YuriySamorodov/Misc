ddfd2fa56c05510bdd75049643a1d5e0475a4f21
Yuriy Samorodov <Yuriy.Samorodov@gmail.com>
2016-11-09 13:50:13 +0300

    Create New-ScheduledJob.ps1

---
+++ New-ScheduledJob.ps1
@@ -0,0 +1,46 @@
+$PSScriptPath = 'C:\Scripts\New-CertificateBackup.ps1'
+
+
+$NewJobTriggerParamerters = @{
+
+    Daily = $true
+    At = "12:00 am"
+
+}
+
+$Trigger = New-JobTrigger @NewJobTriggerParamerters
+
+
+
+$NewScheduledJobOptionParameters = @{
+
+    RunElevated = $true
+    RequireNetwork = $true
+    StartIfOnBattery = $true
+    ContinueIfGoingOnBattery = $true
+    StartIfIdle = $false
+    StopIfGoingOffIdle = $false
+    DoNotAllowDemandStart = $false
+    MultipleInstancePolicy = 'Queue'
+
+
+}
+
+$Options = New-ScheduledJobOption @NewScheduledJobOptionParameters
+
+
+
+$RegisterScheduledJobParameters = @{
+
+    Name = 'Backup Root Certificates'
+    FilePath = $PSScriptPath
+    Credential = 'test\test'
+    Authentication = 'Default'
+    
+
+
+}
+
+
+
+Register-ScheduledJob @RegisterScheduledJobParameters
