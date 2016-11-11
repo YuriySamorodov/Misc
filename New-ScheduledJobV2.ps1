75ec485ccc595e22390fb87cce7601cdc6bc4a4c
Yuriy Samorodov <Yuriy.Samorodov@gmail.com>
2016-11-11 04:24:52 +0300

    Update New-ScheduledJobV2.ps1
    Fixed ScriptBlock issue

--- New-ScheduledJobV2.ps1
+++ New-ScheduledJobV2.ps1
@@ -1,10 +1,142 @@
-﻿# $PSScriptPath = 'C:\Scripts\New-CertificateBackup.ps1'
+#$PSScriptPath = 'C:\Scripts\New-CertificateBackup.ps1'
+
+#Get-Credential -Credential 
+
+#region Credentials
+
+$UserName = 'support'
+$Password = 'mvsupport'
+
+$SecurePasswordParameters = [psobject] @{
+
+        String = $Password
+        AsPlainText = $true
+        Force = $true
+
+}
 
 
-$CertificateBackupScript = 
+$SecurePassword = ConvertTo-SecureString @SecurePasswordParameters
+
+
+$AdminCredentialParameters = [psobject] @{
+
+        TypeName = 'System.Management.Automation.PSCredential'
+        ArgumentList = ( $UserName , $SecurePassword )
+        
+}
+
+
+$AdminCredential = New-Object @AdminCredentialParameters
+
+
+#endregion
+
+#region Certificate Backup
+
+$ScriptBlock = [scriptblock]::Create(
+
+
+    {
+
+
+        BEGIN {
+
+            $Date = Get-Date
+            $FileType = '*.cer'
+            $BackupPathServer01 = 'C:\Certificates'
+            $BackupPathServer02 = 'C:\Certificates1'
+            $CertificatePath = 'Cert:\LocalMachine\Root'
+            $RootCertificates = Get-ChildItem -Path $CertificatePath
+
+    
+        }
+
+        PROCESS {   
+
+        #region New-CertificateBackup
+        #Reading Certificate store and creating *.cer files
+    
+            foreach ( $Certificate in $RootCertificates ) {
+              
+                $JoinPathParameters = @{
+
+                    ChildPath = @(
+                        $($Date.ToString("yyyyMMddhhmm") )
+                        "$($Certificate.Thumbprint).cer"
+                        ) -join "+"
+                    
+                    Path = $BackupPathServer01
+
+
+                }
+                
+                $CerFilePath = Join-Path @JoinPathParameters
+
+                $export = ( $Certificate.Export( 'CERT' ) )
+
+                [System.IO.File]::WriteAllBytes( $CerFilePath, $export )
+                                               
+
+            }
+
+        #endregion
+
+        #region Copy-Item
+        #Copy certificate files to DC02   
+    
+            $CopyItemProperties = @{
+        
+                Path = $BackupPathServer01
+                Destination = $BackupPathServer02
+                Filter =  $FileType
+                Recurse = $true
+                Container = $false
+                Force = $true
+        
+            } 
+
+            Copy-Item @CopyItemProperties
+
+        #endregion
+
+        #region Remove-OldCertificates
+        #Removing 5 days old certificates from both DC01 and DC02
+    
+            $Files = Get-ChildItem $BackupPathServer01, $BackupPathServer02
+            $TresholdDate = $Date.AddDays( -5 )
+            
+            foreach ( $File in $Files ) {
+
+                if ( $File.CreationTime -le $TresholdDate ) {
+                
+                    $RemoveItemProperties = @{
+
+                        Path = $File.FullName
+                        Confirm = $false
+                        Force = $true
+
+                    }
+
+                    Remove-Item @RemoveItemProperties
+
+                }
+
+            }
+    
+        #endregion
+
+        }
+
+        END {
 
 
 
+        }
+
+    }
+  
+)
 
 $NewJobTriggerParamerters = @{
 
@@ -14,8 +146,6 @@ $NewJobTriggerParamerters = @{
 }
 
 $Trigger = New-JobTrigger @NewJobTriggerParamerters
-
-
 
 $NewScheduledJobOptionParameters = @{
 
@@ -28,81 +158,41 @@ $NewScheduledJobOptionParameters = @{
     DoNotAllowDemandStart = $false
     MultipleInstancePolicy = 'Queue'
 
-
 }
 
 $Options = New-ScheduledJobOption @NewScheduledJobOptionParameters
 
-$AdminCredential = Get-Credential
+
+$ErrorActionPreference = 'Stop'
 
 $RegisterScheduledJobParameters = @{
 
     Name = 'Backup Root Certificates'
-    # FilePath = $PSScriptPath
+    #FilePath = $PSScriptPath
     Credential = $AdminCredential
     Authentication = 'Default'
     Confirm = $false
-    Erroraction = 'Stop'
-    #ScriptBlock = $CertificateBackupScript
+    #Erroraction = 'Stop'
+    ScheduledJobOption = $Options
+    RunNow = $true
+    ScriptBlock =  $ScriptBlock
+    
+}
+
+
+
+
+try { 
+ 
+     Register-ScheduledJob @RegisterScheduledJobParameters
+    
+}
+
+catch {
+        
+    Start-Sleep -Seconds 15
+    Unregister-ScheduledJob $RegisterScheduledJobParameters.Name
 
 
 }
 
-
-try {
-
-    Register-ScheduledJob @RegisterScheduledJobParameters -ScriptBlock {`
-    
-     $FileType = '*.cer' ; $BackupPathDC01 = 'C:\Certificates' ; 
-     $BackupPathDC02 = "Microsoft.PowerShell.Core\FileSystem::\\DC02\Certificates" ; 
-     $CertificatePath = 'Cert:\LocalMachine\Root' ; 
-     $RootCertificates = Get-ChildItem -Path $CertificatePath ; 
-     
-     foreach ( $Certificate in $RootCertificates ) { 
-        
-        $CerFilePath = "$BackupPathDC01\$( ( Get-Date -Format "yyyyMMddhhmm" ) + '-'  + $Certificate.Thumbprint ).cer" ; 
-        [System.IO.File]::WriteAllBytes( $CerFilePath , ( $Certificate.Export( 'CERT' ) ) ) } ; 
-        Copy-Item -Path $BackupPathDC01 -Destination $BackupPathDC02 -Filter  $FileType -Recurse $true -Container:$false -Force:$true ;
-        $Files = ( Get-ChildItem -Path $BackupPathDC01 ) + ( Get-ChildItem -Path $BackupPathDC02 ) ; 
-        $Date = ( Get-Date ).AddDays( - 5 ) ;
-        
-        foreach ( $File in $Files ) { 
-            if ( $File.CreationTime -le $Date ) { 
-            Remove-Item  -Path $File.FullName  -Confirm:$false -Force 
-            } 
-        }
-    
-     }
-    
-    }
-
-catch { 
-
-    Unregister-ScheduledJob $RegisterScheduledJobParameters.Name
-    #Start-Sleep -Seconds 2
-    Register-ScheduledJob @RegisterScheduledJobParameters -ScriptBlock {`
-    
-     $FileType = '*.cer' ; $BackupPathDC01 = 'C:\Certificates' ; 
-     $BackupPathDC02 = "Microsoft.PowerShell.Core\FileSystem::\\DC02\Certificates" ; 
-     $CertificatePath = 'Cert:\LocalMachine\Root' ; 
-     $RootCertificates = Get-ChildItem -Path $CertificatePath ; 
-     
-     foreach ( $Certificate in $RootCertificates ) { 
-        
-        $CerFilePath = "$BackupPathDC01\$( ( Get-Date -Format "yyyyMMddhhmm" ) + '-'  + $Certificate.Thumbprint ).cer" ; 
-        [System.IO.File]::WriteAllBytes( $CerFilePath , ( $Certificate.Export( 'CERT' ) ) ) } ; 
-        Copy-Item -Path $BackupPathDC01 -Destination $BackupPathDC02 -Filter  $FileType -Recurse $true -Container:$false -Force:$true ;
-        $Files = ( Get-ChildItem -Path $BackupPathDC01 ) + ( Get-ChildItem -Path $BackupPathDC02 ) ; 
-        $Date = ( Get-Date ).AddDays( - 5 ) ;
-        
-        foreach ( $File in $Files ) { 
-            if ( $File.CreationTime -le $Date ) { 
-            Remove-Item  -Path $File.FullName  -Confirm:$false -Force 
-            } 
-        }
-    
-     }
-
-
-
-    }
