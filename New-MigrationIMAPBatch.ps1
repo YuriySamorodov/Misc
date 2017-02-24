3360380c99d44193da14b6db373064803aa49ae4
Yuriy Samorodov <Yuriy.Samorodov@gmail.com>
2017-02-24 23:35:43 +0300

    Update New-MigrationIMAPBatch.ps1

--- New-MigrationIMAPBatch.ps1
+++ New-MigrationIMAPBatch.ps1
@@ -2,16 +2,21 @@ $file = 'D:\Users\ysamorodov\OneDrive\Downloads\57079+SSPC+ImapTasks.csv' ;
 $TempFilePath = $HOME
 $csv = import-csv $file
 $csv2 = $csv | ConvertTo-Csv -NoTypeInformation
+[byte[]]$headerBytes =  [System.Text.Encoding]::ASCII.GetBytes($($csv2[0]))
+[byte[]]$lineBreak = [System.Text.Encoding]::ASCII.GetBytes("`r`n")
 
 for ( $i = ( $csv2.Count - 1 ) ; $i -le ( $csv2.Count - 1 ) ; $i++ ) {
     Out-File -InputObject $csv2[0,$i] -FilePath $Home\csv.csv -Force  ;
-    New-MigrationBatch -Name "$($csv[$i-1].EmailAddress)" -AutoStart:$false -CSVData:([System.IO.File]::ReadAllBytes("$Home\csv.csv")) -SourceEndpoint mail.sspc.org -NotificationEmails 'ysamorodov@.net' 
+    New-MigrationBatch -Name "$($csv[$i-1].EmailAddress)" -AutoStart:$false -CSVData:([System.IO.File]::ReadAllBytes("$Home\csv.csv")) -SourceEndpoint mail.sspc.org -NotificationEmails '.net' 
 }
 
-for ( $i = ( $csv2.Count - 1 ) ; $i -le ( $csv2.Count - 1 ) ; $i++ ) {
-    [byte[]]$bytes = [System.Text.Encoding]::Unicode.GetBytes("$($csv2[0,41])") ;
-    [byte[]]$bytes = [System.Text.Encoding]::ASCII.GetBytes($($csv2[0])) + 13 + 10 + [System.Text.Encoding]::ASCII.GetBytes($($csv2[41]))
+for ( $i = ( $csv2.Count - 1 ) ; $i -lt $csv2.Count ; $i++ ) {
+    [byte[]]$MigrationUserBytes = [System.Text.Encoding]::ASCII.GetBytes($($csv2[$i])) ;
+    #[byte[]]$bytes = [System.Text.UnicodeEncoding]::ASCII.GetBytes($($csv2[0],"`r`n",$csv2[41])) ;
+    [byte[]]$bytes = $headerBytes + $lineBreak + $MigrationUserBytes
+    
+
     #Out-File -InputObject $csv2[0,$i] -FilePath $Home\csv.csv -Force  ;
-    New-MigrationBatch -Name "$($csv[$i-1].EmailAddress)" -AutoStart:$false -CSVData:($bytes -split ' ') -SourceEndpoint mail.sspc.org -NotificationEmails 'ysamorodov@intermedia.net' 
+    New-MigrationBatch -Name "$($csv[$i-1].EmailAddress)" -AutoStart:$false -CSVData:($bytes) -SourceEndpoint mail.sspc.org -NotificationEmails 'ysamorodov@intermedia.net' 
 }
 
