d2ba08841b80ac0f594f48914738ba4775cb5afc
Yuriy Samorodov <Yuriy.Samorodov@gmail.com>
2017-04-07 23:20:52 +0300

    Create New-MoveRequestBatch.ps1

---
+++ New-MoveRequestBatch.ps1
@@ -0,0 +1,29 @@
+$Users =  Get-MsolUser | where { $_.isLicensed -eq $true }
+$TargetDeliveryDomain = 'YCG01.mail.onmicrosoft.com'
+$EndPoint = Get-MigrationEndpoint
+$csv = $Users | select @{ n = 'EmailAddress' ; e = { $_.UserprincipalName } } | ConvertTo-Csv -NoTypeInformation
+[byte[]]$headerBytes =  [System.Text.Encoding]::ASCII.GetBytes($($csv[0]))
+[byte[]]$lineBreak = [System.Text.Encoding]::ASCII.GetBytes("`r`n")
+
+for ( $i = 0 ; $i -lt $Users.Count ; $i++ ) {
+
+    $MigrationBatchParameters = @{
+
+        Name = $Users[$i].UserprincipalName
+        SourceEndpoint = $EndPoint.Identity
+        TargetDeliveryDomain = $TargetDeliveryDomain
+        #Users = $Users[$i].UserprincipalName
+        CSVData = $headerBytes + $lineBreak + [System.Text.Encoding]::ASCII.GetBytes($($Users[$i].UserprincipalName))
+        BadItemLimit = 100
+        NotificationEmails = 'ysamorodov@intermedia.net'
+        AutoStart = $true
+        AutoComplete = $false
+        AllowIncrementalSyncs = $true
+        DisallowExistingUsers = $true
+        ErrorAction = "Stop"
+        WhatIf = $false
+        Verbose = $true
+    }
+
+    New-MigrationBatch @MigrationBatchParameters 
+}
