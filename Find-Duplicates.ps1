40182cada8da81b8b77f3697fa7d516be78a7af1
Yuriy Samorodov <Yuriy.Samorodov@gmail.com>
2017-10-17 12:46:51 +0300

    Add files via upload

---
+++ Find-Duplicates.ps1
@@ -0,0 +1,13 @@
+﻿$msolUsers = Get-MsolUser | sort DisplayName, isLicensed
+
+for ( $i = 1 ; $i -lt $msolUsers.Count ; $i++ ) {
+    while ( $msolUsers[$i].Displayname -eq $msolUsers[$i-1].Displayname  ) {
+        if ( $msolUsers[$i].isLicensed -eq $false ) {
+            $msolusers[$i-1].ProxyAddresses += $msolUsers[$i].UserPrincipalname
+        }
+        elseif ( $msolUsers[$i-1].isLicensed -eq $false ) {
+            $msolusers[$i].ProxyAddresses += $msolUsers[$i-1].UserPrincipalname
+        }
+        $i++
+    }
+}
