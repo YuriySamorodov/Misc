d19d02a8fd68998de9b63686da8504493ccae865
Yuriy Samorodov <Yuriy.Samorodov@gmail.com>
2017-10-17 13:06:10 +0300

    Update Find-Duplicates.ps1

--- Find-Duplicates.ps1
+++ Find-Duplicates.ps1
@@ -1,12 +1,16 @@
-﻿$msolUsers = Get-MsolUser | sort DisplayName, isLicensed
+[System.Collections.ArrayList]$msolUsers = Get-MsolUser | sort DisplayName, isLicensed
 
-for ( $i = 1 ; $i -lt $msolUsers.Count ; $i++ ) {
-    while ( $msolUsers[$i].Displayname -eq $msolUsers[$i-1].Displayname  ) {
+for ( $i = 0 ; $i -lt $msolUsers.Count ; $i++ ) {
+    while ( ( $msolUsers[$i].Displayname -eq $msolUsers[$i-1].Displayname ) -or ( $msolUsers[$i].Displayname -eq $msolUsers[$i+1].Displayname ) -or ( $msolUsers[-1].DisplayName -eq $msolUsers[$i-2].DisplayName ) ) {
         if ( $msolUsers[$i].isLicensed -eq $false ) {
-            $msolusers[$i-1].ProxyAddresses += $msolUsers[$i].UserPrincipalname
+            $msolusers[$i-1].ProxyAddresses.Add("smtp:$($msolUsers[$i].UserPrincipalname)")
+            $msolUsers.RemoveAt($i)
+            Write-Output "Removing $( $msolUsers[$i].DisplayName )"
         }
         elseif ( $msolUsers[$i-1].isLicensed -eq $false ) {
-            $msolusers[$i].ProxyAddresses += $msolUsers[$i-1].UserPrincipalname
+            $msolusers[$i].ProxyAddresses.Add("smtp:$($msolUsers[$i-1].UserPrincipalname)")
+            $msolUsers.RemoveAt($i-1)
+            Write-Output "Removing $( $msolUsers[$i].DisplayName )"
         }
         $i++
     }
