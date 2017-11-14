44ff591b9032491612c61ce7bc89079f3ad7f6b2
Yuriy Samorodov <Yuriy.Samorodov@gmail.com>
2017-11-14 20:39:00 +0300

    Add files via upload

---
+++ New-O365MigrationEndpointRPCManual.ps1
@@ -0,0 +1,29 @@
+﻿$RPCProxyServer = 'west.exch025.serverdata.net'
+$ExchangeServer = 'west.exch025.serverdata.net'
+$UserName = 'Migration@homefixcr.com'
+$EmailAddress = 'tryan@homefixcr.com'
+$Pass = 'Password1'
+
+$SecurePasswordParameters = [psobject] @{
+        String = $Pass
+        AsPlainText = $true
+        Force = $true
+    }
+    $SecurePassword = ConvertTo-SecureString @SecurePasswordParameters
+
+    $AdminCredentialParameters = [psobject] @{
+        TypeName = 'System.Management.Automation.PSCredential'
+        ArgumentList = ( $UserName , $SecurePassword ) 
+    }
+    $AdminCredential =  New-Object @AdminCredentialParameters
+
+
+$NewMigrationEndPoint = @{
+    Name = $RPCProxyServer
+    ExchangeOutlookAnywhere = $true
+    RPCProxyServer = $RPCProxyServer
+    ExchangeServer = $ExchangeServer
+    Credentials = $AdminCredential
+    EmailAddress = 
+}
+New-MigrationEndpoint @NewMigrationEndPoint -Name -PublicFolder
