31545fa6b87c40a923fab5526ec8914c58bf0c31
Yuriy Samorodov <Yuriy.Samorodov@gmail.com>
2017-12-13 18:03:38 +0300

    Added Module selection

--- New-Office365Session.ps1
+++ New-Office365Session.ps1
@@ -1,79 +1,87 @@
 function New-Office365Session {
 
-    param (
-        
+    param (    
         [parameter(Mandatory = $true, Position=1)]
         [string]$UserName = '',
         [parameter(Mandatory = $true, Position=2)]
-        [string]$Password =  '',
+        [string]$Pass =  '',
         [parameter(Mandatory = $false, Position=3)]
+        [ValidateSet('AzureAD', 'Exchange', 'Compliance', 'SharePoint','Skype','All')]
+        [string]$Module =  'Exchange',
+        [parameter(Mandatory = $false, Position=4)]
         [string]$Account =  ''
-
     )
 
+    $URIExchangeOnline = 'https://ps.outlook.com/powershell-LiveID'
+    $URICompliance = 'https://ps.compliance.protection.outlook.com/powershell-LiveID'
 
-    if ( $Account -notmatch "." ) {
-        
-        $ConnectionURI = "https://ps.outlook.com/powershell-LiveID"
-
-    }
-
-    else {
-        $ConnectionURI = "https://ps.outlook.com/powershell-LiveID?DelegatedOrg=$Account.onmicrosoft.com" 
-    }
-
-    $SecurePasswordParameters = [psobject] @{
-
-        String = $Password
-        AsPlainText = $true
-        Force = $true
-
-    }
-
-
-    $SecurePassword = ConvertTo-SecureString @SecurePasswordParameters
-
-
-    $AdminCredentialParameters = [psobject] @{
-
-        TypeName = 'System.Management.Automation.PSCredential'
-        ArgumentList = ( $UserName , $SecurePassword )
-        
-    }
-
-
-    $AdminCredential =  New-Object @AdminCredentialParameters
-
-    $ExchangeSessionParameters = [psobject] @{
-
-        ConnectionURI = $ConnectionURI
-        ConfigurationName = 'Microsoft.Exchange'
-        Authentication = 'Basic'
-        AllowRedirection = $true    
-        Credential = $AdminCredential
-        #AllowClobber = $true
-
-    }
-
-    $ExchangeSession = New-PSSession @ExchangeSessionParameters
-
-    Import-PSSession $ExchangeSession -AllowClobber
-
-
-    $ComplianceSessionParameters = [psobject] @{
-
-        ConnectionURI = $ConnectionURI
-        ConfigurationName = 'Microsoft.Exchange'
-        Authentication = 'Basic'
-        AllowRedirection = $true    
-        Credential = $AdminCredential
-        #AllowClobber = $true
-
-    }
-
-    $ComplianceSession = New-PSSession @ComplianceSessionParameters
-
-    Import-PSSession $ComplianceSession -AllowClobber
+    function Set-O365Credentials {
     
+        $SecurePasswordParameters = [psobject] @{
+            String = $Pass
+            AsPlainText = $true
+            Force = $true
+        }
+        $SecurePassword = ConvertTo-SecureString @SecurePasswordParameters
+    
+        $AdminCredentialParameters = [psobject] @{
+            TypeName = 'System.Management.Automation.PSCredential'
+            ArgumentList = ( $UserName , $SecurePassword ) 
+        }
+        $AdminCredential =  New-Object @AdminCredentialParameters
+    
+    }
+
+        Set-O365Credentials
+
+    function Connect-OExchange {
+    
+        if ( $Account -notmatch "." ) {
+            $ConnectionURI = $URIExchangeOnline
+        }
+        else {
+            $ConnectionURI = $URIExchangeOnline + "/?DelegatedOrg=$Account.onmicrosoft.com" 
+        }
+
+    
+        $ExchangeSessionParameters = [psobject] @{
+            ConnectionURI = $ConnectionURI
+            ConfigurationName = 'Microsoft.Exchange'
+            Authentication = 'Basic'
+            AllowRedirection = $true    
+            Credential = $AdminCredential
+            #AllowClobber = $true
+        }
+        $ExchangeSession = New-PSSession @ExchangeSessionParameters
+        Import-PSSession $ExchangeSession -AllowClobber
+
+    }
+
+    function Connect-SecurityAndCompliance {
+
+        $URICompliance = 'https://ps.compliance.protection.outlook.com/powershell-LiveID'
+    
+        $ComplianceSessionParameters = [psobject] @{
+            ConnectionURI = $URICompliance
+            ConfigurationName = 'Microsoft.Exchange'
+            Authentication = 'Basic'
+            AllowRedirection = $true    
+            Credential = $AdminCredential
+            #AllowClobber = $true
+        }
+        $ComplianceSession = New-PSSession @ComplianceSessionParameters
+        Import-PSSession $ComplianceSession -AllowClobber
+    }
+
+    function Connect-All {
+        Connect-Exchange
+        Connect-SecurityAndCompliance    
+    }
+    
+    switch ($Module) {
+        'All' { Connect-All }
+        'Exchange' { Connect-Exchange  }
+    }
 
 }
+
