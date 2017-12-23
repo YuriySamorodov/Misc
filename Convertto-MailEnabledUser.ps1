59e517b1173ce68de8ea80e2ce99d7ebfc3dd745
Yuriy Samorodov <Yuriy.Samorodov@gmail.com>
2017-12-23 03:49:18 +0300

    Update Convertto-MailEnabledUser.ps1

--- Convertto-MailEnabledUser.ps1
+++ Convertto-MailEnabledUser.ps1
@@ -1,6 +1,6 @@
 foreach ( $row in $csv ) { 
     $mailbox = Get-Mailbox $row.IntermediaEmail ; 
-    $EmailAddreses = $EmailAddreses = ( $mailbox.EmailAddresses | ForEach-Object { $_.ProxyAddressString } ) + "X500:$($mailbox.LegacyExchangeDN)"; 
+    $( $mailbox.EmailAddresses | ForEach-Object { $_.ProxyAddressString } ) + "X500:$($mailbox.LegacyExchangeDN)"; 
     Disable-Mailbox -Identity $mailbox.Identity -Confirm:$false ; 
     Enable-MailUser -Identity $mailbox.Identity -PrimarySmtpAddress $mailbox.PrimarySmtpAddress -ExternalEmailAddress $row.OnMicrosoftEmailAddress -Confirm:$false ; 
     Set-MailUser -Identity $mailbox.Identity -EmailAddressPolicyEnabled $false -EmailAddresses $EmailAddreses 
