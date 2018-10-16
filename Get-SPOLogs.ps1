457872d68cff1f58a768ed792f7b42ef1e08b5a7
Yuriy Samorodov <Yuriy.Samorodov@gmail.com>
2018-10-16 18:56:29 +0300

    Update Get-SPOLogs.ps1

--- Get-SPOLogs.ps1
+++ Get-SPOLogs.ps1
@@ -4,20 +4,8 @@ $resultSize = 5000
 [datetime]$startDate = '10/09/2018'
 [datetime]$endDate = '10/13/2018'
 $auditData = @() ;
-function SearchUnifiedAuditLog () {
-    param (
-        [parameter(Mandatory=$true, Position = 1)]
-        [guid]$searchGuid,
-        [parameter(Mandatory=$true, Position = 2)]
-        [datetime]$startDate,
-        [parameter(Mandatory=$true, Position = 3)]
-        [datetime]$endDate,
-        [parameter(Mandatory=$true, Position = 4)]
-        [int]$resultSize,
-        [parameter(Mandatory=$false, Position = 5)]
-        [int]$Timeout
-    )
-    
+function SearchUnifiedAuditLog {
+
     $SearchUnifiedAuditLogParameters = @{
         SessionCommand = 'ReturnLargeSet'
         SessionId = $searchGuid
@@ -28,12 +16,12 @@ function SearchUnifiedAuditLog () {
     Search-UnifiedAuditLog @SearchUnifiedAuditLogParameters ;
 }
 
-$auditData += SearchUnifiedAuditLog $searchGuid $startDate $endDate $resultSize
+$auditData += SearchUnifiedAuditLog
 Start-Sleep -Milliseconds 500
 
 do {
 Write-Host "$($auditData[-1].ResultIndex)"
-$auditData += SearchUnifiedAuditLog $searchGuid $startDate $endDate $resultSize
+$auditData += SearchUnifiedAuditLog
 Start-Sleep -Milliseconds 500
 }
 while ( $auditData.Count % $resultSize -eq 0 ) ;
