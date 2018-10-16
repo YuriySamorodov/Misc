7122c4e1ae845b56e94cfccce037ad2231c27651
Yuriy Samorodov <Yuriy.Samorodov@gmail.com>
2018-10-16 18:53:04 +0300

    Update Get-SPOLogs.ps1

--- Get-SPOLogs.ps1
+++ Get-SPOLogs.ps1
@@ -1,13 +1,40 @@
-$start = Get-Date ; 
-$resultSize = 5000 ;
-[datetime]$startDate = '09/12/2018'
+$start = Get-Date ;
+$searchGuid = New-Guid
+$resultSize = 5000
+[datetime]$startDate = '10/09/2018'
 [datetime]$endDate = '10/13/2018'
-$auditData = @() ; 
-$auditData = Search-UnifiedAuditLog -RecordType SharePoint, SharePointFileOperation, SharePointSharingOperation -SessionCommand ReturnLargeSet -EndDate $endDate -StartDate $startDate -SessionId "SPOLogSearch" -ResultSize $resultSize  ;
-do { 
+$auditData = @() ;
+function SearchUnifiedAuditLog () {
+    param (
+        [parameter(Mandatory=$true, Position = 1)]
+        [guid]$searchGuid,
+        [parameter(Mandatory=$true, Position = 2)]
+        [datetime]$startDate,
+        [parameter(Mandatory=$true, Position = 3)]
+        [datetime]$endDate,
+        [parameter(Mandatory=$true, Position = 4)]
+        [int]$resultSize,
+        [parameter(Mandatory=$false, Position = 5)]
+        [int]$Timeout
+    )
+    
+    $SearchUnifiedAuditLogParameters = @{
+        SessionCommand = 'ReturnLargeSet'
+        SessionId = $searchGuid
+        StartDate = $startDate
+        EndDate = $endDate
+        ResultSize = $resultSize
+    }
+    Search-UnifiedAuditLog @SearchUnifiedAuditLogParameters ;
+}
+
+$auditData += SearchUnifiedAuditLog $searchGuid $startDate $endDate $resultSize
+Start-Sleep -Milliseconds 500
+
+do {
 Write-Host "$($auditData[-1].ResultIndex)"
-$auditData += Search-UnifiedAuditLog -RecordType SharePoint, SharePointFileOperation, SharePointSharingOperation -SessionCommand ReturnLargeSet -EndDate $endDate -StartDate $startDate -SessionId "SPOLogSearch" -ResultSize $resultSize ; 
-Start-Sleep -Milliseconds 500 
-} 
-while ( $auditData.Count % $resultSize -eq 0 ) ; 
-$end = Get-Date 
+$auditData += SearchUnifiedAuditLog $searchGuid $startDate $endDate $resultSize
+Start-Sleep -Milliseconds 500
+}
+while ( $auditData.Count % $resultSize -eq 0 ) ;
+$end = Get-Date
