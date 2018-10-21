58dcad0ed5be9eb100cc5c4b8a1efdd5dbd153d5
Yuriy Samorodov <Yuriy.Samorodov@gmail.com>
2018-10-21 14:39:43 +0300

    Update Get-SPOLogs.ps1

--- Get-SPOLogs.ps1
+++ Get-SPOLogs.ps1
@@ -1,33 +1,50 @@
-$searchGuid = New-Guid
+$start = Get-Date
+
 $resultSize = 5000
-[datetime]$startDate = '10/09/2018'
-[datetime]$endDate = '10/13/2018'
+[datetime]$startDate = '07/22/2018 00:00:00'
+[datetime]$endDate = '10/20/2018 00:00:00'
+$currentStart = $null
+$CurrentEnd = $null
+$Interval = 15
 $recordType = @(
     'SharePoint',
     'SharepointFileOperation',
     'SharePointSharingOperation'
 )
-$auditData = @() ;
-function SearchUnifiedAuditLog {
+$results = @() ;
 
-    $SearchUnifiedAuditLogParameters = @{
+function SearchUnifiedAuditLog {
+    $global:SearchUnifiedAuditLogParameters = @{
         SessionCommand = 'ReturnLargeSet'
-        SessionId = $searchGuid
-        StartDate = $startDate
-        EndDate = $endDate
-        RecordType = $recordType
-        ResultSize = $resultSize
+        SessionId = $SessionId
+        StartDate = $CurrentStart
+        EndDate = $CurrentEnd
+        #RecordType = [string]($recordType -join ',')
+        ResultSize = $ResultSize
     }
     Search-UnifiedAuditLog @SearchUnifiedAuditLogParameters ;
 }
 
-$auditData = SearchUnifiedAuditLog
-Start-Sleep -Milliseconds 500
 
-do {
-Write-Host "$($auditData[-1].ResultIndex)"
-$auditData += SearchUnifiedAuditLog
-Start-Sleep -Milliseconds 500
-}
-while ( $auditData.Count % $resultSize -eq 0 ) ;
+#$auditData = SearchUnifiedAuditLog
+#Start-Sleep -Milliseconds 500
+#Write-Host "$($auditData[-1].ResultCount)"
+do{
+    $auditData = @()
+    $SessionId = New-Guid
+    do {
+        if ( $currentStart -eq $null ) {
+            $currentStart = $startDate
+        }
+        $CurrentEnd = $currentStart.AddMinutes($Interval)
+        $auditData += SearchUnifiedAuditLog
+        Write-Host "$($CurrentStart): $($auditData[-1].ResultIndex)"
+        Start-Sleep -Milliseconds 500
+        }
+        while ( $auditData.Count % $resultSize -eq 0 ) 
+    $results += $auditData
+    $currentStart = $currentEnd
+    }
+until ( $currentStart -eq $endDate )
 
+$end = Get-Date
