d7b4eeffc7782cb00500b00a7f76f2382e92a357
Yuriy Samorodov <yuriy.samorodov@veeam.com>
2018-10-27 01:59:17 +0300

    Almost done

--- Get-SPOLogsTest.ps1
+++ Get-SPOLogsTest.ps1
@@ -1,3 +1,4 @@
+$scriptStart = Get-Date
 Get-Job | Remove-Job
 $currentStart  = $null
 $currentEnd = $null
@@ -7,7 +8,7 @@ $ResultSize = 5000
 $LogPath = 'C:\Users\yury.samorodov\Downloads\SPOLogs'
 $StartDate = Get-Date
 $StartDate = $StartDate.Date.AddHours(-48)
-$EndDate = $StartDate.AddHours(24)
+$EndDate = $StartDate.AddHours(1)
 $ConnectionURI = "https://ps.outlook.com/powershell-LiveID/?proxymethod=RPS"
 
 do  {
@@ -15,14 +16,16 @@ do  {
     if ($CurrentStart -eq $null) {
             $CurrentStart = $StartDate
         }
-    
+
+    $jobs = @()
+    Get-Job | Remove-Job
     #Getting log data in an hour timespan using 15 minutes chunks
-    $scriptStart = Get-Date
+
     for ($i = 0 ; $i -lt 60) {
         $AuditData = New-Object System.Collections.ArrayList
         $SessionId = New-Guid
         $CurrentEnd = $CurrentStart.AddMinutes($interval)
-        $JobName = "SPOLogs_$($CurrentStart.ToString("yyyyMMddHHmm"))"
+        $JobName = "SPOLogs_$($CurrentStart.ToString("yyyyMMddHH"))00"
         $SearchUnifiedAuditLogParameters = @{
             SessionId = $SessionId
             StartDate = $CurrentStart
@@ -30,21 +33,33 @@ do  {
             SessionCommand = 'ReturnLargeSet'
             FreeText = "sharepoint\.com"
             ResultSize = $ResultSize
+            ErrorAction = 'Stop'
             #OutVariable = '+data'
         }
-        Start-Job -Name $JobName -ScriptBlock {
+        $jobs += Start-Job -Name $JobName -ScriptBlock {
             param ($PassedArgs)
             $data = New-Object System.Collections.ArrayList
-            Search-UnifiedAuditLog @PassedArgs -outvariable +data
+            #Search-UnifiedAuditLog @PassedArgs -outvariable +data
             #Write-Host $data
             do {
+                #$ErrorActionPreference = 'Stop'
                 #$AuditData += SearchUnifiedAuditLog -CurrentStart $CurrentStart -CurrentEnd $CurrentEnd
-                Search-UnifiedAuditLog @PassedArgs -outvariable +data
-                $localResults = Get-Job | Receive-Job -Keep
+                try {
+                    Search-UnifiedAuditLog @PassedArgs -outvariable +data
+                }
+                catch {
+                    #Write-Host $($error[0])
+                    Get-PSSession | Remove-PSSession
+                    Connect-Exchange 'svcexchlogcollector@veeam.com' 'LuB&BN0GIrWV'
+                    Search-UnifiedAuditLog @PassedArgs -outvariable +data
+                }
+                finally {
+                    $localResults = Get-Job | Receive-Job -Keep
+                }
             } while ( $localResults[-1].ResultIndex -ne $localResults[-1].ResultCount )
         } -InitializationScript {
             Import-Module .\Connect-Exchange.ps1 ;
-            Connect-Exchange 'svcexchlogcollector@veeam.com' 'LuB&BN0GIrWV' ;
+            #Connect-Exchange 'svcexchlogcollector@veeam.com' 'LuB&BN0GIrWV' ;
             #Connect-Exchange 'yuriy.samorodov@veeam.com' 'K@znachey' ;
             #Import-Module .\Search-O365UnifiedAuditLogs.ps1 ;
         } -ArgumentList $SearchUnifiedAuditLogParameters,$data
@@ -54,7 +69,7 @@ do  {
         $i = $i + $interval
         $CurrentStart = $CurrentEnd
     }
-    $scriptEnd = Get-Date
+    
 
     if ($jobs.Count -eq 12) {
         $jobs | Wait-Job | Out-Null
@@ -69,8 +84,10 @@ do  {
         }
         Get-Job | Remove-Job
         Start-Sleep -Seconds 60
-        $Jobs = @()
+        #$Jobs = @()
     }
 } while ( 
     $currentStart -le $EndDate
 )
+$scriptEnd = Get-Date
+Write-Host "Total Run time: $($scriptEnd - $scriptStart"
