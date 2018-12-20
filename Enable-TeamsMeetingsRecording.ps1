84f25996b3dac072b5396ee6ac6bbf230b132cd7
Yuriy Samorodov <yuriy.samorodov@veeam.com>
2018-12-20 22:53:20 +0300

    Added a bunch of splatted tables

--- Enable-TeamsMeetingsRecording.ps1
+++ Enable-TeamsMeetingsRecording.ps1
@@ -7,3 +7,39 @@
 - add user to DSG.O365.Stream.VideoUploaders group
 #>
 
+function connectMSOnline {
+    param (
+        [pscredential]$Credential
+    )
+    $Credential = Get-Credential
+    Connect-Msolservice -Credential $Credential
+}
+
+connectMSOnline
+
+function enableTeamsMeetingRecording {
+    param (
+        
+    )
+    
+    #remove Stream Trial License VeeamSoftwareCorp:STREAM
+    $StreamTrialLicense = Get-MsolAccountSku | Where-Object { $_.AccountSkuId -match ":STREAM$" }
+    $EnterprisePack = Get-MsolAccountSku | Where-Object { $_.AccountSkuId -match ":ENTERPRISEPACK$" }
+    $Plans = $EnterprisePack.ServiceStatus
+    $DisabledPlans = $Plans | Where-Object { $_.ProvisioningStatus -eq 'Disabled' }
+    $DisabledPlans = $DisabledPlans | Where-Object { $_.ServicePlan.ServiceName -notmatch "STREAM" }
+    $LicenseOptionProperties = @{
+        AccountSkuId = $EnterprisePack.AccountSkuId
+        DisabledPlans = $DisabledPlans
+    }
+    $LicenseOption = New-MsolLicenseOptions @LicenseOptionProperties
+    
+    $SetMsolUserLicenseProperties = @{
+        UserPrincipalName = 'yuriy.samorodov@veeam.com'
+        RemoveLicenses = $StreamTrialLicense
+        LicenseOptions = $LicenseOption
+    }
+    #Set-MsolUserLicense @SetMsolUserLicenseProperties
+    Set-MsolUserLicense -UserPrincipalName 'yuriy.samorodov@veeam.com' -RemoveLicenses $StreamTrialLicense -LicenseOptions $LicenseOption
+    
+}
