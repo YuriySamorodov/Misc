c96182ab745df5bda0613149867f948f88cab152
Yuriy Samorodov <yuriy.samorodov@veeam.com>
2019-10-25 20:02:30 +0300

    Check Exchange Agent Log Status

--- Get-ADGroupsRecursive.ps1
+++ Get-ADGroupsRecursive.ps1
@@ -2,13 +2,14 @@
     param (
         [string]$Identity
     )
+    #$ErrorActionPreference = 'Stop'
     $members = Get-ADGroupMember -Identity $Identity
     for ( $i = 0 ; $i -lt $members.Count ; $i++ ) {
         if ( $members[$i].ObjectClass -eq 'group' ) {
             Get-ADGroupMemberRecursive -Identity $members[$i].Name
         }
         if ( $members[$i].ObjectClass -eq 'user') {
-            $member = Get-ADObject -Filter "Name -eq '$($members[$i].Name)'" -Properties *  
+            $member = Get-ADUser -LDAPFilter "(&(Name=$($members[$i].Name))(!(UserAccountControl:1.2.840.113556.1.4.803:=2)))"
             $objProps = [ordered]@{}
             for ( $p = 0 ; $p -lt $member.PropertyCount ; $p++){
                 $property = ([array]$member.PropertyNames)[$p]
